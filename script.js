79a52f96e973d050cfc30e02785b918b320d9a88
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-10 21:56:21 +0100

    added array

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // card options
+    const cardArray = [
+        {
+            names: 'train',
+            img: 'images/img3.png'
+        },
+        {
+            name: 'boat',
+            img: 'images/img4.png'
+        },
+        {
+            name: 'water',
+            img: 'images/img5.png'
+        },
+        {
+            name: 'forest',
+            img: 'images/img6.png'
+        },
+        {
+            name: 'flower',
+            img: 'images/img7.png'
+        },
+        {
+            name: 'temple',
+            img: 'images/img8.png'
+        }
+    ]
+})
